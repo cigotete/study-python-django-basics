b9824ec9c87b0e7804582dd64d62428048e2ede4
Camilo Figueroa <cigotete@gmail.com>
2024-01-22 17:07:21 -0500

    Removed comments.

--- helloworld/posts/models.py
+++ helloworld/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Post(models.Model): # new
+class Post(models.Model):
   text = models.TextField()
 
-  def __str__(self): # new
+  def __str__(self):
     return self.text[:50]
