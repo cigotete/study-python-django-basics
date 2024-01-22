5d982ad27ee1ef51acfed6af8ebc0b20fa0f196a
Camilo Figueroa <cigotete@gmail.com>
2024-01-22 18:53:04 -0500

    VIew and template for individual blog post.

--- helloworld/blog/urls.py
+++ helloworld/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
   path("blog", BlogListView.as_view(), name="blog"),
+  path("blog/post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
 ]
