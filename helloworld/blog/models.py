94ca0605ae28607297b7f91bc85685db9e5f9e8b
Camilo Figueroa <cigotete@gmail.com>
2024-01-23 18:47:31 -0500

    Updated Blog model to use new custom user model.

--- helloworld/blog/models.py
+++ helloworld/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
@@ -5,7 +6,7 @@ class Post(models.Model):
   title = models.CharField(max_length=200)
   body = models.TextField()
   author = models.ForeignKey(
-    "auth.User",
+    settings.AUTH_USER_MODEL,
     on_delete=models.CASCADE,
   )
 
